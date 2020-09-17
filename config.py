a4129577c308a95a09d2f262e89bcfa34b650c9c
Lourine <lourine.millicent@gmail.com>
2020-09-17 12:13:19 +0300

    Regiister and login User

--- config.py
+++ config.py
@@ -5,6 +5,18 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wangari:7890@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    print(MAIL_USERNAME)
+    print(MAIL_PASSWORD)
 
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 export MOVIE_API_KEY='c25720bba7177580b070e5c8638512bf'
 export SECRET_KEY='1234milly'
 
+export MAIL_USERNAME="lourinemilly@gmail.com"
+export MAIL_PASSWORD="2017twins"
+
 python3.8 manage.py server
